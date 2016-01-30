536821bd00c3d352f25afd11c459012bc44048f5
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-30 17:45:33 +0600

    Записали основные параметры css, начали писать шапку

---
+++ mySite/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Новости обо всём</title>
+	<link href="/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header>
+		<div id="logo">
+        
+        </div>
+        <div id="menuHead">
+        
+        </div>
+        <div id="regAuth">
+        
+        </div>
+	</header>
+</body>
+</html>
