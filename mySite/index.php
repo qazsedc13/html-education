0dc64ebdc7a51fcef9acde762542ecd96661b4d7
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-30 22:35:22 +0600

    Создали футер

--- mySite/index.php
+++ mySite/index.php
@@ -23,5 +23,25 @@
         	<a href="/reg.php">Регистрация</a> | <a href="/auth.php">Авторизация</a>
         </div>
 	</header>
+    
+<footer>
+    	<div id="social">
+        	<a href="http://vk.com" title="Группа ВК" target="_blank">
+            	<img src="/img/vk.png" alt="ВК" title="ВК">
+            </a>
+            <a href="http://ok.ru" title="Группа Одноклассники" target="_blank">
+            	<img src="/img/ok.png" alt="Одноклассники" title="Одноклассники">
+            </a>
+            <a href="http://facebook.com" title="Группа Facebook" target="_blank">
+            	<img src="/img/facebook.png" alt="Facebook" title="Facebook">
+            </a>
+            <a href="http://twitter.com" title="Группа Twitter" target="_blank">
+            	<img src="/img/twitter.png" alt="Twitter" title="Twitter">
+            </a>
+        </div>
+        <div id="rights">
+	      Все права защищены &copy;<?=date ('Y')?>
+    	</div>
+</footer>
 </body>
 </html>
