b02102865a14ca983a03415829bf27e639bf82b9
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-31 21:33:18 +0600

    Создали блоки сайтов. Подключили их с помощью php. В будующем будет удобно

--- mySite/index.php
+++ mySite/index.php
@@ -1,28 +1,14 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Новости обо всём</title>
-	<link href="/css/style.css" rel="stylesheet" type="text/css">
+	<?php
+		$title = "Новости обо всем";
+		require_once "blocks/head.php"
+	?>
 </head>
 
 <body>
-	<header>
-		<div id="logo">
-        	<a href="/" title="Перейти на главную"><span>Н</span>овости</a>
-        </div>
-        <div id="menuHead">
-        	<a href="/about.php">
-            	<div style="margin-right:5%">О нас</div>
-            </a>
-            <a href="/feedback.php">
-            	<div>Обратная связь</div>
-            </a>
-        </div>
-        <div id="regAuth">
-        	<a href="/reg.php">Регистрация</a> | <a href="/auth.php">Авторизация</a>
-        </div>
-	</header>
+	<?php require_once "blocks/header.php"?>
     <div id="wrapper">
     	<div id="leftCol">
         	<div id="bigArticle">
@@ -51,33 +37,8 @@
                 </a>
             </div>            
         </div>
-        <div id="rightCol">
-        	<div class="banner">
-            	<img src="/img/banner_1.gif" alt="Баннер 1" title="Баннер 1" />
-            </div>
-            <div class="banner">
-            	<img src="/img/banner_2.jpg" alt="Баннер 2" title="Баннер 2" />
-            </div>      
-        </div>    
+        <?php require_once "blocks/rightCol.php"?>  
     </div>    
-	<footer>
-    	<div id="social">
-        	<a href="http://vk.com" title="Группа ВК" target="_blank">
-            	<img src="/img/vk.png" alt="ВК" title="ВК">
-            </a>
-            <a href="http://ok.ru" title="Группа Одноклассники" target="_blank">
-            	<img src="/img/ok.png" alt="Одноклассники" title="Одноклассники">
-            </a>
-            <a href="http://facebook.com" title="Группа Facebook" target="_blank">
-            	<img src="/img/facebook.png" alt="Facebook" title="Facebook">
-            </a>
-            <a href="http://twitter.com" title="Группа Twitter" target="_blank">
-            	<img src="/img/twitter.png" alt="Twitter" title="Twitter">
-            </a>
-        </div>
-        <div id="rights">
-	      Все права защищены &copy;<?=date ('Y')?>
-    	</div>
-	</footer>
+	<?php require_once "blocks/footer.php"?>
 </body>
 </html>
