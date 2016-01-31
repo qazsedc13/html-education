964846a4ad16038b4fc25c19ace6af797eb5b90d
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-31 16:00:45 +0600

    Закончили боковую панель

--- mySite/index.php
+++ mySite/index.php
@@ -29,7 +29,7 @@
         </div>
         <div id="rightCol">
         	<div class="banner">
-            	<img src="/img/banner_1.jpg" alt="Баннер 1" title="Баннер 1" />
+            	<img src="/img/banner_1.gif" alt="Баннер 1" title="Баннер 1" />
             </div>
             <div class="banner">
             	<img src="/img/banner_2.jpg" alt="Баннер 2" title="Баннер 2" />
