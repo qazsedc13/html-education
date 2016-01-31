2037d95282102b152eaa9b5fd25796ba1bb17a16
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-31 16:16:00 +0600

    Добавили первую статью

--- mySite/index.php
+++ mySite/index.php
@@ -25,7 +25,16 @@
 	</header>
     <div id="wrapper">
     	<div id="leftCol">
-        	s
+        	<div id="bigArticle">
+            	<img src="/img/article_1.jpg" alt="Статья 1" title="Статья 1" />
+                <h2>Статья 1</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div>Далее</div>
+            </div>
+            <div class="clear"></div>
+            <div class="article">
+            
+            </div>
         </div>
         <div id="rightCol">
         	<div class="banner">
