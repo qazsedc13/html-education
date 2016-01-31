41c1833604a2d321110be85da95ae6e8957c294b
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-31 22:09:35 +0600

    Создали форму отправки сообщения

---
+++ mySite/feedback.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+	<?php
+		$title = "Обратная связь";
+		require_once "blocks/head.php"
+	?>
+</head>
+
+<body>
+	<?php require_once "blocks/header.php"?>
+    <div id="wrapper">
+    	<div id="leftCol">
+			<input type="text" placeholder="Имя" id="name" name="name" /><br />
+            <input type="text" placeholder="Email" id="email" name="email" /><br />
+            <input type="text" placeholder="Тема сообщения" id="subject" name="subject" /><br />
+            <textarea name="message" id="message"></textarea><br /> 
+            <input type="button" name="done" id="done" value="Отправить" /><br />     
+        </div>
+        <?php require_once "blocks/rightCol.php"?>  
+    </div>    
+	<?php require_once "blocks/footer.php"?>
+</body>
+</html>
