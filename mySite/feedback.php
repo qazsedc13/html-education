21c9e2672570f4e7bfca61b2a08715123d2d0b04
qazsedc13 <bgnikkov@rambler.ru>
2016-02-01 11:43:30 +0600

    Сделали проверку формы

--- mySite/feedback.php
+++ mySite/feedback.php
@@ -5,6 +5,31 @@
 		$title = "Обратная связь";
 		require_once "blocks/head.php"
 	?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script>
+    	$(document).ready(function() {
+            $("#done").click (function (){
+				$('#messageShow').hide ();
+				var name = $("#name").val();
+				var email = $("#email").val();
+				var subject = $("#subject").val();
+				var message = $("#message").val();
+				var fail = "";
+				if (name.length < 3) fail = "Имя не меньше 3 символов";
+				else if (email.split('@').length - 1 == 0|| email.split('.').length - 1 == 0)
+					fail = "Вы ввели некорректный email"	;
+				else if (subject.length < 5)
+					fail = "Тема сообщения менее 5 символов";	
+				else if (message.length < 2)
+					fail = "Сообщение не менее 20 символов";
+				if (fail != "") {
+					$('#messageShow').html(fail + "<div class='clear'><br /></div>");
+					$('#messageShow').show ();
+					return false;	
+				}
+			});
+        });
+    </script>
 </head>
 
 <body>
@@ -15,6 +40,7 @@
             <input type="text" placeholder="Email" id="email" name="email" /><br />
             <input type="text" placeholder="Тема сообщения" id="subject" name="subject" /><br />
             <textarea name="message" id="message" placeholder="Введите сюда ваше сообщение"></textarea><br /> 
+            <div id="messageShow"></div>
             <input type="button" name="done" id="done" value="Отправить" /><br />     
         </div>
         <?php require_once "blocks/rightCol.php"?>  
