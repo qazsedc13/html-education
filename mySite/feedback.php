484555bdcba6d0d1bfd07cfef49e1ce6cefeb605
qazsedc13 <bgnikkov@rambler.ru>
2016-02-01 11:13:42 +0600

    Создали страницу обратной связи и немного украсили её

--- mySite/feedback.php
+++ mySite/feedback.php
@@ -14,7 +14,7 @@
 			<input type="text" placeholder="Имя" id="name" name="name" /><br />
             <input type="text" placeholder="Email" id="email" name="email" /><br />
             <input type="text" placeholder="Тема сообщения" id="subject" name="subject" /><br />
-            <textarea name="message" id="message"></textarea><br /> 
+            <textarea name="message" id="message" placeholder="Введите сюда ваше сообщение"></textarea><br /> 
             <input type="button" name="done" id="done" value="Отправить" /><br />     
         </div>
         <?php require_once "blocks/rightCol.php"?>  
