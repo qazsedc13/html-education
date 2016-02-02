891f1654f06d4633988644a42c77f7b095eb9636
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-02-02 21:11:58 +0600

    Удалили лишнее, то что было закоментировано

--- mySite/index.php
+++ mySite/index.php
@@ -31,32 +31,7 @@
 					
 			}
 		?>
-        
-        	<!--<div id="bigArticle">
-            	<img src="/img/articles/1.jpg" alt="Статья 1" title="Статья 1" />
-                <h2>Статья 1</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <a href="/article.php">
-                	<div class="more">Далее</div>
-                </a>
-            </div>
-            <div class="clear"><br /></div>
-            <div class="article">
-            	<img src="/img/articles/2.jpg" alt="Статья 1" title="Статья 1" />
-                <h2>Статья 1</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                <a href="/article.php">
-                	<div class="more">Далее</div>
-                </a>
-            </div>
-            <div class="article">
-            	<img src="/img/articles/3.jpg" alt="Статья 1" title="Статья 1" />
-                <h2>Статья 1</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                <a href="/article.php">
-                	<div class="more">Далее</div>
-                </a>
-            </div>-->            
+                	          
         </div>
         <?php require_once "blocks/rightCol.php"?>  
     </div>    
