6ef7b3d52a3232cde243ca9b913d09074ea53546
qazsedc13 <bgnikkov@rambler.ru>
2016-02-05 10:25:41 +0600

    Добавили тег для лучшей адаптивности сайта

--- mySite/blocks/head.php
+++ mySite/blocks/head.php
@@ -1,4 +1,8 @@
 	<meta charset="utf-8">
+    
+    <!-- Adopt website to current screen -->
+	<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    
 	<title><?=$title?></title>
 	<link href="/css/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
